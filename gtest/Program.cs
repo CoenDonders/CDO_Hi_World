72b3b055e5eec2abbdd2abfb8257f839babad75d
coen <coen.donders@resulth.nl>
2020-12-28 11:19:43 +0100

    1

---
+++ gtest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gtest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
